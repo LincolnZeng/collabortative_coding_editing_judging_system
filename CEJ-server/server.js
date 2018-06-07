37a2c9c9ec0386dc588f1b827ba6a0f143c77e1f
ZengLincoln <zenglincoln@gmail.com>
2018-06-07 15:07:02 -0700

    revert git

---
+++ CEJ-server/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var port = 3000;
+
+app.use("api/v1", restRouter);
+app.listen(port, function () {
+    console.log('APP is listening to ' + port);
+});
