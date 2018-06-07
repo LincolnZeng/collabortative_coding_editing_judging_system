079ec196da5462c37930f3a69f0ed1b02cff577b
ZengLincoln <zenglincoln@gmail.com>
2018-06-07 15:54:57 -0700

    add moogose and data schema

--- CEJ-server/server.js
+++ CEJ-server/server.js
@@ -5,5 +5,5 @@ var port = 3000;
 
 app.use("api/v1", restRouter);
 app.listen(port, function () {
-    console.log('APP is listening to ' + port + 'we are good to go!');
+    console.log('APP is listening to ' + port + ' we are good to go!');
 });
