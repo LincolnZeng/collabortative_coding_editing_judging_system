5e84c5dc9fcc29d84266be9250949a6df564699c
ZengLincoln <zenglincoln@gmail.com>
2018-06-07 14:51:46 -0700

    add back server(node): server.js, rest.js(express router), problemService(node promise)

--- CEJ-client/src/app/app.module.ts
+++ CEJ-client/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { DataService } from './services/data.service';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- CEJ-client/src/app/services/data.service.ts
+++ CEJ-client/src/app/services/data.service.ts
@@ -5,14 +5,21 @@ import { PROBLEMS } from '../mock-problem';
 @Injectable()
 export class DataService {
 
+  problems: Problem[] = PROBLEMS;
+
   constructor() { }
 
   getProblems(): Problem[] {
-    return PROBLEMS;
+    return this.problems;
   }
 
   getProblem(id:number): Problem {
-    return PROBLEMS.find(problem => problem.id === id);
+    return this.problems.find(problem => problem.id === id);
+  }
+
+  addProblem(newProblem:Problem): void{
+    newProblem.id = this.problems.length + 1;
+    this.problems.push(newProblem);
   }
 
 }
