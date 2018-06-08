acdfbe35fb34bf20309a84e2156b31e3b404440f
ZengLincoln <zenglincoln@gmail.com>
2018-06-07 20:59:08 -0700

    Revert "connect to mongodb mlab"
    This reverts commit a95d590fe2cb18a9b1585aa19faf06f0a765009a.

--- CEJ-server/server.js
+++ CEJ-server/server.js
@@ -2,15 +2,8 @@ var express = require('express');
 var app = express();
 var restRouter = require('./routes/rest');
 var port = 3000;
-var mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://CEJ_database:Yx736089!@ds151970.mlab.com:51970/cej_database');
-//mongoose.connect("mongodb://cej:cej001@ds151970.mlab.com:51970/cej");
-mongoose.connect('mongodb://user:user@ds037587.mlab.com:37587/coj');
-
-app.use("/api/v1/", restRouter);
-
-
+app.use("api/v1", restRouter);
 app.listen(port, function () {
-    console.log('APP is listening to ' + port);
+    console.log('APP is listening to ' + port + ' we are good to go!');
 });
