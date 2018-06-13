611d46bebff7543bb656eb03763a5560fb5e7235
ZengLincoln <zenglincoln@gmail.com>
2018-06-13 11:57:41 -0700

    add ace editor and use socketIo to implement collaborative editing

---
+++ oj-client/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
+import { CollaborationService } from './services/collaboration.service';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EditorComponent } from './components/editor/editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavBarComponent,
+    EditorComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [
+    {
+    provide: "data",
+    useClass: DataService
+},
+    {
+      provide: "collaboration",
+      useClass: CollaborationService
+    }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
