3d27371743e5db2c7353136eb82b5d9787e340fe
ZengLincoln <zenglincoln@gmail.com>
2018-06-14 16:27:40 -0700

    modigy launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
 fuser -k 3000/tcp
-fuser -k 5000/tcp
 
 brew services start redis
-cd ./oj-server
-npm install
-nodemon server.js &
-cd ../oj-client
+
+cd ./CEJ-client
 npm install
 ng build --watch &
-cd ../executor
-pip install -r requirements.txt
-python executor_server.py &
+cd ../CEJ-server
+npm install
+nodemon server.js
 
-echo "=================================================="
-read -p "PRESS [ENTER] TO TERMINATE PROCESSES." PRESSKEY
+echo "============================================"
+read -p "PRESS [ENTER] TO TERMINATE PROCESS." PRESSKEY
 
 fuser -k 3000/tcp
-fuser -k 5000/tcp
 brew services stop redis
